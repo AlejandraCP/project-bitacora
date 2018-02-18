d9973f90e7fa5d5e12f47f4c5bfe4772ab06f556
AlejandraCP <alejandracp710@gmail.com>
2018-02-18 16:04:37 -0500

    Adding function to post text

--- js/app.js
+++ js/app.js
@@ -1,20 +1,24 @@
 $("#modal1").modal();
 
+let $btnPost = $(".btn-post");
+let $title = $("#title");
+let $message = $("#message");
+let $boxContainer = $(".post-container");
 let $btnMessage = $('.btn-message');
 
-let $title = $('#title');
-let $message = $('#message');
-
-let $boxContainer = $('.post-container');
-
-
-
 const postMessage = () => {
-  let template = `<div class="container shadow">
-  <h3>${$title.val()}</h3>
-  </div>`
-  console.log('fs');
+  if ($title.val() !== "" && $message.val() !== '') {
+  let template = `<div class="container box-container z-depth-3">
+  <h4>${$title.val()}</h4>
+  <p>${$message.val()}</p>
+  </div>`;
   $boxContainer.prepend(template);
-  $title.val('');
-}
-$btnMessage.click(postMessage);
+  $title.val("");
+  $message.val("");
+  } 
+  else {
+    alert('Escribe en todos los espacios');
+  }
+};
+
+$btnPost.click(postMessage);
