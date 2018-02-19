1b4862d7cd2310c11b2bab718b9f1b22e8d60790
AlejandraCP <alejandracp710@gmail.com>
2018-02-19 02:04:25 -0500

    Adding function post-date complete

--- js/app.js
+++ js/app.js
@@ -1,38 +1,46 @@
+$('.datepicker').pickadate({
+  selectMonths: true, // Creates a dropdown to control month
+  today: 'Today',
+  clear: 'Clear',
+  close: 'Ok',
+  closeOnSelect: false // Close upon selecting a date,
+});
+
 // Api google maps
 var map;
-// function initMap() {
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: -34.397, lng: 150.644},
-//     zoom: 17
-//   });
-//   var infoWindow = new google.maps.InfoWindow({map: map});
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 17
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
 
-//   // Try HTML5 geolocation.
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(function(position) {
-//       var pos = {
-//         lat: position.coords.latitude,
-//         lng: position.coords.longitude
-//       };
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
-//       infoWindow.setPosition(pos);
-//       infoWindow.setContent('Location found.');
-//       map.setCenter(pos);
-//     }, function() {
-//       handleLocationError(true, infoWindow, map.getCenter());
-//     });
-//   } else {
-//     // Browser doesn't support Geolocation
-//     handleLocationError(false, infoWindow, map.getCenter());
-//   }
-// }
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
 
-// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//   infoWindow.setPosition(pos);
-//   infoWindow.setContent(browserHasGeolocation ?
-//                         'Error: The Geolocation service failed.' :
-//                         'Error: Your browser doesn\'t support geolocation.');
-// }
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}
 
 $("#modal1").modal();
 $("#modal2").modal();
@@ -44,8 +52,8 @@ let $boxContainer = $(".post-container");
 let $titleDate = $('#title-date');
 let $inputDate = $('#date');
 let $btnMessage = $('.btn-message');
-let $btnDate = $('.btn-date');
-let $btnPostDate = $('.btn-post-date');
+let $btnDate = $('#btn-date');
+let $btnPostDate = $('btn-post-date');
 
 const postMessage = () => {
   if ($title.val() !== "" && $message.val() !== '') {
@@ -67,9 +75,10 @@ const postDate = () => {
     let template = `<div class="container box-container z-depth-3">
   <h4>${$titleDate.val()}</h4>
   <p>${$inputDate.val()}</p>
-  </div>
   <div id="map">
-  </div>`;
+  </div>
+  </div>
+  `;
     $boxContainer.prepend(template);
     $titleDate.val("");
     $inputDate.val("");
@@ -80,40 +89,8 @@ const postDate = () => {
 };
 
 $btnPost.click(postMessage);
-$btnPostDate.click(postDate);
-$btnPostDate.click(function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 17
-  });
-  var infoWindow = new google.maps.InfoWindow({ map: map });
-
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      map.setCenter(pos);
-    }, function () {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-},
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-      'Error: The Geolocation service failed.' :
-      'Error: Your browser doesn\'t support geolocation.');
-  });
+$btnDate.click(postDate);
 
 
-
+var btnPost = document.getElementById('btn-date');
+btnPost.addEventListener('click', initMap);
